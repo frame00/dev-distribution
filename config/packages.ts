dbc5b59a808b5ad9b4aa97516ba00818797cc9c8
aggre <hiroyuki.aggre@gmail.com>
2018-08-15 08:56:50 +0900

    Add target package

--- config/packages.ts
+++ config/packages.ts
@@ -1,3 +1,9 @@
 import { DistributionTarget } from '../src/types'
 
-export const packages: DistributionTarget[] = []
+export const packages: DistributionTarget[] = [
+	{
+		package: 'zengin-code',
+		address: '0x623527a95416f2F451565976bB50e97050267e8D',
+		date: '2018-08-15'
+	}
+]
