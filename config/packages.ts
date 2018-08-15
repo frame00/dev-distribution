d155268e96d316c30943b3e08fbc82757d7ec7ee
aggre <hiroyuki.aggre@gmail.com>
2018-08-15 22:53:10 +0900

    Add Kazupon's target packages

--- config/packages.ts
+++ config/packages.ts
@@ -5,5 +5,25 @@ export const packages: DistributionTarget[] = [
 		package: 'zengin-code',
 		address: '0x623527a95416f2F451565976bB50e97050267e8D',
 		date: '2018-08-15'
+	},
+	{
+		package: 'vue-i18n',
+		address: '0x69cc2c86aeb26f52f6645a2dfdec1051dd5584c0',
+		date: '2018-08-15'
+	},
+	{
+		package: 'vue-i18n-extensions',
+		address: '0x69cc2c86aeb26f52f6645a2dfdec1051dd5584c0',
+		date: '2018-08-15'
+	},
+	{
+		package: '@kazupon/vue-i18n-loader',
+		address: '0x69cc2c86aeb26f52f6645a2dfdec1051dd5584c0',
+		date: '2018-08-15'
+	},
+	{
+		package: 'vue-cli-plugin-i18n',
+		address: '0x69cc2c86aeb26f52f6645a2dfdec1051dd5584c0',
+		date: '2018-08-15'
 	}
 ]
