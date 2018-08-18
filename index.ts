57f800a442a88b0884df2766da1ed3db974933f1
aggre <hiroyuki.aggre@gmail.com>
2018-08-18 11:52:11 +0900

    Write logs to file system

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { packages } from './config/packages'
 import app from './src/index'
+import { writeFile } from 'fs'
 
 const [, , start, end, total] = process.argv
+const DIR = 'log'
+const write = (path: string, content: string) =>
+	writeFile(path, content, 'utf-8', () => console.log(content))
 
 app(start, end, ~~total, packages)
-	.then(res => console.log(res))
+	.then(res => write(`${DIR}/${start}-to-${end}.json`, JSON.stringify(res)))
 	.catch(err => console.error(err))
