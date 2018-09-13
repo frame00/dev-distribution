3362de8d10e85c736de24f24202573d33687d21e
aggre <hiroyuki.aggre@gmail.com>
2018-09-14 00:30:59 +0900

    Create server

---
+++ serve/index.ts
@@ -0,0 +1,29 @@
+import micro, { createError } from 'micro'
+import { parse } from 'url'
+import { packages } from '../config/packages'
+import { contract } from '../config/contract'
+import { distribution } from '../config/distribution'
+
+const error = (status = 404, body = 'not found') => createError(status, body)
+
+micro(async req => {
+	const { url } = req
+	if (!url) {
+		throw error()
+	}
+	const parsed = parse(url)
+	const { pathname } = parsed
+	if (!pathname) {
+		throw error()
+	}
+	switch (pathname) {
+		case '/config/packages':
+			return packages
+		case '/config/contract':
+			return contract
+		case '/config/distribution':
+			return distribution
+		default:
+			throw error()
+	}
+}).listen(3000)
