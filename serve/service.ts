efcbe89aede00925840ccac7f830060799b0c9aa
aggre <hiroyuki.aggre@gmail.com>
2018-11-29 23:46:48 +0900

    fix: incorrect return type

--- serve/service.ts
+++ serve/service.ts
@@ -1,13 +1,16 @@
-import { createError } from 'micro'
+import { createError, send as _send } from 'micro'
 import { parse } from 'url'
 import { packages } from '../config/packages'
 import { contract } from '../config/contract'
 import { distribution } from '../config/distribution'
-import { IncomingMessage } from 'http'
+import { IncomingMessage, ServerResponse } from 'http'
 
 const error = (status = 404, body = 'not found') => createError(status, body)
+const sendCreator = (res: ServerResponse) => async <T>(body: T) =>
+	_send(res, 200, body)
 
-export const service = async (req: IncomingMessage) => {
+export const service = async (req: IncomingMessage, res: ServerResponse) => {
+	const send = sendCreator(res)
 	const { url } = req
 	if (!url) {
 		throw error()
@@ -19,11 +22,11 @@ export const service = async (req: IncomingMessage) => {
 	}
 	switch (pathname) {
 		case '/config/packages':
-			return packages
+			return send(packages)
 		case '/config/contract':
-			return contract
+			return send(contract)
 		case '/config/distribution':
-			return distribution
+			return send(distribution)
 		default:
 			throw error()
 	}
