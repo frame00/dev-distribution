eef437ad89e8ee4cf9f7ad74e830066f840ff97f
aggre <hiroyuki.aggre@gmail.com>
2019-02-20 18:13:59 +0900

    add npm packages owned d0whc3r

--- config/packages.ts
+++ config/packages.ts
@@ -264,5 +264,11 @@ export const packages: DistributionTarget[] = [
 		address: '0x7129FD777858CDC8A27cb7627F7Cd81C60d4C194',
 		user: 'samuil4',
 		date: '2019-02-16'
+	},
+	{
+		package: '@d0whc3r/vue-auth-plugin',
+		address: '0x1ec4Df7e235f7309d88870b8D00F3Da2f899350d',
+		user: 'd0whc3r',
+		date: '2019-02-20'
 	}
 ]
